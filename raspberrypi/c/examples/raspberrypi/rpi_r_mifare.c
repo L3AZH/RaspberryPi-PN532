df314589805df70f7664a2bb9b14a0c60928ee27
L3azh <lamhatuananh6@gmail.com>
2025-08-26 23:08:49 +0700

    Update code read mifare

--- raspberrypi/c/examples/raspberrypi/rpi_r_mifare.c
+++ raspberrypi/c/examples/raspberrypi/rpi_r_mifare.c
@@ -1,5 +1,5 @@
 /**
-  * This example shows connecting to the PN532 and writing an Mifare Classic
+  * This example shows connecting to the PN532 and writing a Mifare Classic
   * type RFID tag
   */
 #include <stdio.h>
@@ -8,49 +8,66 @@
 #include "pn532.h"
 #include "pn532_rpi.h"
 
-void resetBuff(uint8_t* buff){
-    for (uint8_t i=0; i < 255; i++) {
+void resetBuff(uint8_t *buff) {
+    for (uint8_t i = 0; i < 255; i++) {
         buff[i] = 0x00;
     }
 }
 
+void readBlockNumber(
+    PN532 pn532,
+    uint8_t *uid,
+    uint8_t uid_length,
+    uint8_t *key_a,
+    uint8_t blockNumber,
+    uint8_t *buff) {
+    uint32_t pn532_error = PN532_ERROR_NONE;
+    printf("block %d \n", blockNumber);
+    pn532_error = PN532_MifareClassicAuthenticateBlock(&pn532, uid, uid_length,
+                                                       blockNumber, MIFARE_CMD_AUTH_A, key_a);
+    if (pn532_error) {
+        printf("Error when authenticating: 0x%02x\r\n", pn532_error);
+        return;
+    }
+    pn532_error = PN532_MifareClassicReadBlock(&pn532, buff, blockNumber);
+    if (pn532_error) {
+        printf("Error when reading: 0x%02x\r\n", pn532_error);
+    }
+    for (uint8_t j = 0; j < MIFARE_BLOCK_LENGTH; j++) {
+        printf("%02x ", buff[j]);
+    }
+    printf("\n");
+    resetBuff(buff);
+
+    printf("========\n");
+}
+
 void readFullDataMifareClassic1K(
     PN532 pn532,
-    uint8_t* uid,
+    uint8_t *uid,
     uint8_t uid_length,
-    uint8_t* key_a)
-{
+    uint8_t *key_a) {
     uint8_t buff[255];
     //init buff with 0x00 character
     resetBuff(buff);
 
     uint32_t pn532_error = PN532_ERROR_NONE;
-    for (uint8_t i=0; i < sizeof(buff); i++){
-        buff[i]=0x00;
+    for (uint8_t i = 0; i < sizeof(buff); i++) {
+        buff[i] = 0x00;
     }
-    // Read data of 4N block
-    for (uint8_t i=1; i<16 ; i++){
-        printf("block %d \n", i*4);
-        pn532_error = PN532_MifareClassicAuthenticateBlock(&pn532, uid, uid_length,
-                    i*4, MIFARE_CMD_AUTH_A, key_a);
-        if (pn532_error) {
-            printf("Error when authenticating: 0x%02x\r\n", pn532_error);
-            return;
-        }
-        pn532_error = PN532_MifareClassicReadBlock(&pn532, buff, i*4);
-        if (pn532_error) {
-            printf("Error when reading: 0x%02x\r\n", pn532_error);
-        }
-        for (uint8_t j=0; j < MIFARE_BLOCK_LENGTH; j++) {
-            printf("%02x ", buff[j]);
-        }
-        printf("\n");
-        resetBuff(buff);
+    // Read data of 4N, 4N+1, 4N+2 block
+    for (uint8_t i = 1; i < 16; i++) {
+        //block 4N
+        readBlockNumber(pn532, uid, uid_length, key_a, i * 4, buff);
+        //block 4N+1
+        readBlockNumber(pn532, uid, uid_length, key_a, i * 4 + 1, buff);
+        //block 4N+2
+        readBlockNumber(pn532, uid, uid_length, key_a, i * 4 + 2, buff);
     }
 }
 
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     uint8_t buff[255];
     uint8_t uid[MIFARE_UID_MAX_LENGTH];
     uint8_t key_a[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
@@ -68,8 +85,7 @@ int main(int argc, char** argv) {
     }
     PN532_SamConfiguration(&pn532);
     printf("Waiting for RFID/NFC card...\r\n");
-    while (1)
-    {
+    while (1) {
         // Check if a card is available to read
         uid_len = PN532_ReadPassiveTarget(&pn532, uid, PN532_MIFARE_ISO14443A, 1000);
         if (uid_len == PN532_STATUS_ERROR) {
@@ -98,7 +114,7 @@ int main(int argc, char** argv) {
     uint8_t block_number = 6;
     //uint8_t DATA[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
     pn532_error = PN532_MifareClassicAuthenticateBlock(&pn532, uid, uid_len,
-            block_number, MIFARE_CMD_AUTH_A, key_a);
+                                                       block_number, MIFARE_CMD_AUTH_A, key_a);
     if (pn532_error) {
         printf("Error: 0x%02x\r\n", pn532_error);
         return -1;
@@ -106,4 +122,3 @@ int main(int argc, char** argv) {
 
     readFullDataMifareClassic1K(pn532, uid, uid_len, key_a);
 }
-
